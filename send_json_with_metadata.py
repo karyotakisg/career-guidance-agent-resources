9b1511e6d6be1bc0d324a7520cc0398de36740b5
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-12-10 19:27:24 +0200

    use metadataFields instead of tags

---
+++ send_json_with_metadata.py
@@ -0,0 +1,60 @@
+import json
+import requests
+import sys
+from dotenv import load_dotenv
+import os
+
+choice = sys.argv[1]
+if choice == 'undergraduates':
+    file = 'undergraduates/undergraduates2.json'
+    chunk_name = 'undergraduates'
+    searchable_fields = ["University","Department","City"]
+    tag = ["undergraduates"]
+elif choice == 'masters':
+    file = 'masters/masters2.json'
+    chunk_name = 'masters'
+    searchable_fields = ["university","department","tuition","duration"]
+    tag = ["masters"]
+else:
+    print("Invalid choice. Please choose 'undergraduates' or 'masters'.")
+    sys.exit(1)
+
+def split_json_array(json_array, chunk_size):
+    """
+    Split a JSON array into smaller arrays of a specified chunk size.
+    """
+    return [json_array[i:i + chunk_size] for i in range(0, len(json_array), chunk_size)]
+
+
+with open(file, 'r', encoding='utf-8-sig') as f:
+    current_json = json.load(f)
+
+
+chunk_size = 200
+json_chunks = split_json_array(current_json, chunk_size)
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+# API URL and headers
+url = f'https://api.voiceflow.com/v1/knowledge-base/docs/upload/table?overwrite=true'
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "Authorization": api_key
+}
+
+# Post each chunk to the API
+for i, chunk in enumerate(json_chunks):
+    payload = { "data": {
+            "name": f'{chunk_name}_{i+1}',
+            "schema": {
+                "searchableFields": searchable_fields,
+                "metadataFields": ["tag"]
+            },
+            "items": chunk
+             } }
+    response = requests.post(url, json=payload, headers=headers)
+    print(f"Chunk {i+1}: Response: {response.status_code}, {response.text}")
+
+print("All chunks have been processed.")
